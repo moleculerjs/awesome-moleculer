f9f8eb8cf97aa2eb639004f1c5fe34757ee1e991
AndMaz <andre.maz.90@gmail.com>
2019-05-21 09:34:22 +0100

    Test travis

---
+++ trigger.sh
@@ -0,0 +1,12 @@
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_TRIGGER" \
+   -d "$body" \
+   https://api.travis-ci.com/repo/AndreMaz%2awesome-moleculer/settings
