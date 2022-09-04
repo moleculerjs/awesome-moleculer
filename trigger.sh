4fe81741c9b807945efd17b40481a1aebaa56b91
AndreMaz <andre.maz.90@gmail.com>
2022-09-04 14:28:37 +0100

    bump ruby

--- trigger.sh
+++ trigger.sh
@@ -11,3 +11,12 @@ curl -s -X POST \
    -H "Authorization: token $TRAVIS_TRIGGER" \
    -d "$body" \
    https://api.travis-ci.org/repo/moleculerjs%2Fsite/requests
+
+# Trigger Website build
+# More info: https://docs.github.com/en/rest/actions/workflows#create-a-workflow-dispatch-event
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \ 
+  -H "Authorization: Bearer <YOUR-TOKEN>" \
+  https://api.github.com/repos/moleculerjs/site/actions/workflows/WORKFLOW_ID/dispatches \
+  -d '{"ref":"master"}'
