163924c9fa39c3fe00acae30a88b2f496086cc4e
AndreMaz <andre.maz.90@gmail.com>
2022-09-04 15:44:09 +0100

    update trigger + push to master

--- trigger.sh
+++ trigger.sh
@@ -1,22 +1,9 @@
-body='{
-"request": {
-   "message":"Trigger from Awesome Repo",
-   "branch":"master"
-}}'
-
-curl -s -X POST \
-   -H "Content-Type: application/json" \
-   -H "Accept: application/json" \
-   -H "Travis-API-Version: 3" \
-   -H "Authorization: token $TRAVIS_TRIGGER" \
-   -d "$body" \
-   https://api.travis-ci.org/repo/moleculerjs%2Fsite/requests
-
 # Trigger Website build
 # More info: https://docs.github.com/en/rest/actions/workflows#create-a-workflow-dispatch-event
-curl \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \ 
-  -H "Authorization: Bearer <YOUR-TOKEN>" \
-  https://api.github.com/repos/moleculerjs/site/actions/workflows/WORKFLOW_ID/dispatches \
-  -d '{"ref":"master"}'
+curl --location --request POST "https://api.github.com/repos/moleculerjs/awesome-moleculer/actions/workflows/ci.yml/dispatches" \
+--header "Accept: application/vnd.github+json" \
+--header "Authorization: Bearer $GH_TRIGGER" \
+--header "Content-Type: application/json" \
+--data-raw '{
+    "ref": "master"
+}'
