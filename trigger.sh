d80943b51512b694190902d49fb1db5b85f5055a
Icebob <mereg.norbert@gmail.com>
2022-09-07 09:18:51 +0200

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,6 +1,6 @@
 # Trigger Website build
 # More info: https://docs.github.com/en/rest/actions/workflows#create-a-workflow-dispatch-event
-curl --location --request POST "https://api.github.com/repos/moleculerjs/awesome-moleculer/actions/workflows/ci.yml/dispatches" \
+curl --location --request POST "https://api.github.com/repos/moleculerjs/site/actions/workflows/ci.yml/dispatches" \
 --header "Accept: application/vnd.github+json" \
 --header "Authorization: Bearer $GH_TRIGGER" \
 --header "Content-Type: application/json" \
